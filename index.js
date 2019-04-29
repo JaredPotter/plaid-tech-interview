3c0dcebbfd3d62abecd168f6e0ac92b1ca779ead
Jared Potter <jaredpotter1@gmail.com>
2019-04-29 16:52:07 -0600

    Problem solutions.

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
 let fs = require('fs');
 
-function main() {
-    console.log('hello, world!');
+function asciiToByte() {
+    const contents = fs.readFileSync('plaid-first-round.txt', 'utf8');
+    const split = contents.split('\n');
+
+    let arr = [];
+
+    split.forEach((line) => {
+        const values = line.substr(1, line.length - 2);
+        const numValues = values.split(',');
+        const x = Number(numValues[0]);
+        const y = Number(numValues[1]);
+        const ascii = Number(numValues[2]);
+
+        arr.push(x)
+        arr.push(y)
+        arr.push(ascii)
+    });    
+
+    const buf = Buffer.from(arr);
+ 
+    // for(const byt of buf.values()){
+    //     console.log(byt);
+    // }
+
+    return buf;
 }
 
-main();
+function createAsciiArt() {
+    const arr = new Array(100);
 
+    for(let i = 0; i < 100; i++) {
+        arr[i] = new Array(100);
+        arr[i].fill(0);
+    }
+
+    const contents = fs.readFileSync('plaid-first-round.txt', 'utf8');
+    const split = contents.split('\n');
+
+    split.forEach((line) => {
+        const values = line.substr(1, line.length - 2);
+        const numValues = values.split(',');
+        const x = Number(numValues[0]);
+        const y = Number(numValues[1]);
+        const ascii = Number(numValues[2]);
+        const char = String.fromCharCode(ascii);
+
+        arr[x][y] = char;
+    });
+
+    for(let i = 0; i < 100; i++) {
+        let result = '';
+
+        for(let j = 0; j < 100; j++) {
+            const value = arr[j][i];
+
+            if(value !== 0) {
+                result += value;
+            }
+            else {
+                result += ' ';
+            }
+        }
+
+        console.log(result);
+    }
+}
+
+createAsciiArt();
+asciiToByte();
+// module.exports = { doTheThing };
--- interview.spec.js
+++ interview.spec.js
@@ -1,14 +1,20 @@
 const assert = require('assert');
+const { doTheThing } =  require('./index');
+
 
 describe('Boilerplate String Tests', function() { // file/class/controller name (general thing being tested).
 
     // possibly additional describes.
 
     it('should return number of characters in a string', function() { // specific expected behavior.
-        assert.equal('hello1'.length, 5); // fails
+        assert.equal('hello1'.length, 6); // 
     });
 
-    it('should return number of characters in a string', function() {
-        assert.equal('bob'.length, 3); // passes
+    it('actual result matches expected result', function() {
+        const expected = 7;
+
+        const actual = doTheThing();
+
+        assert.equal(expected, actual); // passes
     });
 });
